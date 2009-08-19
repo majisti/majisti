ce2da16365655e5c3f7333931948a0c9670dff19
Steven Rosato <steven.rosato@majisti.com>
2009-08-19 13:46:36 +0000

    updated coverage.sh

--- incubator/tests/Majisti/coverage.sh
+++ incubator/tests/Majisti/coverage.sh
@@ -1,2 +1,2 @@
-rm -r ../../../coverage/*
-phpunit --coverage-html ../../../coverage AllTests.php 2>/dev/null
+rm -r ../../../../coverage/*
+phpunit --coverage-html ../../../../coverage AllTests.php 2>/dev/null
