88bd85b6cc6801348ed6b481ee9bec6323dc73ed
Steven Rosato <steven.rosato@majisti.com>
2009-08-21 03:40:26 +0000

    started the quickstart documentation

---
+++ incubator/library/MajistiX/Modules/forums/Bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+class Forums_Bootstrap
+{
+	public function __construct()
+	{
+		print 'foo';
+	}
+}
---
+++ incubator/library/MajistiX/Modules/users/controllers/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+class Users_AuthController extends Zend_Controller_Action
+{
+	public function indexAction()
+	{
+		
+	}
+	
+	public function loginAction()
+	{
+		
+	}
+}
