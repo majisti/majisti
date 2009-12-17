c4ccc3275cd6fbec1288b736375a0828876c739d
Steven Rosato <steven.rosato@majisti.com>
2009-12-17 05:49:04 +0000

    Worked on MajistiX Auth Module
    - Added classes documentation
    - Minor refactoring
    - Code cleanup
    
    What's next:
    
    Ajax processing

--- incubator/library/MajistiX/Modules/auth/models/LastUrl.php
+++ incubator/library/MajistiX/Modules/auth/models/LastUrl.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * @desc Model that will remember an url through the session
+ * 
+ * @author Steven Rosato
+ */
 class Auth_Model_LastUrl
 {
     /**
@@ -7,17 +12,30 @@ class Auth_Model_LastUrl
      */
     protected $_session;
     
+    /**
+     * @desc Constructs the model with a request object.
+     * 
+     * @param $request The request
+     */
     public function __construct(Zend_Controller_Request_Http $request = null)
     {
         $this->_session = new Zend_Session_Namespace('Majistix_Auth_LastUrl');
         $this->setRequest($request);
     }
     
+    /**
+     * @desc Sets the request object.
+     * @param $request The request object
+     */
     public function setRequest(Zend_Controller_Request_Http $request)
     {
         $this->_session->request = $request;
     }
     
+    /**
+     * @desc Redirect according to the request's setup action, controller
+     * and module names.
+     */
     public function gotoLastUrl()
     {
         $redirector = Zend_Controller_Action_HelperBroker::getStaticHelper('redirector');
