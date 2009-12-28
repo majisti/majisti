0fbb5265fe15bbafbb9fc9240f940b404eb64c3a
Steven Rosato <steven.rosato@majisti.com>
2009-12-28 06:20:11 +0000

    added more constants and view helpers path

--- incubator/demo/simple/public/index.php
+++ incubator/demo/simple/public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+define('APPLICATION_NAME', 'Simple');
+
 if( version_compare(PHP_VERSION, '5.3.0') < 0 ) {
 	die("This project is compatible with PHP 5.3 or higher, your version is: " . PHP_VERSION);
 }
@@ -12,4 +14,4 @@ define('MAJISTI_FOLDER_NAME', 'Majisti');
 
 /* PHP parsers below 5.3.0 will understand this, but die in version compare */
 require_once '../application/Loader.php';
-call_user_func('\Majisti\Demo\Simple\Application\Loader::load');
+call_user_func('\Majisti\Demo\\' . APPLICATION_NAME . '\Application\Loader::load');
