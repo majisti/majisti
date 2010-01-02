6350793c7c59c637a8755eaa2af0c491986d1924
Steven Rosato <steven.rosato@majisti.com>
2010-01-02 00:52:00 -0500

    Added two more inits:
    - A Null Translator
    - Application library autoloader

--- library/Majisti/Application/Bootstrap/Bootstrap.php
+++ library/Majisti/Application/Bootstrap/Bootstrap.php
@@ -43,4 +43,36 @@ class Bootstrap extends \Zend_Application_Bootstrap_Bootstrap
 
         return \Zend_Registry::get('Majisti_ModelContainer');
     }
+
+    /**
+     * @desc Inits a null translator.
+     *
+     * @return \Zend_Translate_Adapter_Abstract
+     */
+    protected function _initTranslation()
+    {
+        \Zend_Registry::set('Zend_Translate', new \Zend_Translate_Adapter_Array(
+            array(), null, array('disableNotices' => true)), array());
+
+        return \Zend_Registry::get('Zend_Translate');
+    }
+
+    /**
+     * @desc Inits the application's library autoloader
+     * which is basically the same as an module autoloader.
+     *
+     * @return \Zend_Application_Module_Autoloader
+     */
+    protected function _initLibraryAutoloader()
+    {
+        $autoloader = \Zend_Loader_Autoloader::getInstance();
+        $libAutoloader = new \Zend_Application_Module_Autoloader(array(
+            'basePath'  => APPLICATION_LIBRARY,
+            'namespace' => APPLICATION_NAME
+        ));
+
+        $autoloader->pushAutoloader($libAutoloader);
+
+        return $libAutoloader;
+    }
 }
