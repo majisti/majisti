70174cd9088584d8b19b77ccad6a39ab9a0093e0
Steven Rosato <steven.rosato@majisti.com>
2010-01-02 00:55:21 -0500

    Added a controller action helper model container retrieval.

---
+++ library/Majisti/Controller/Action/Helper/Model.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @desc Controller Helper that returns Majisti's ModelContainer
+ *
+ * @author Majisti
+ */
+class Majisti_Controller_Action_Helper_Model extends Zend_Controller_Action_Helper_Abstract
+{
+    /**
+     * @desc Returns the registered Majisti ModelContainer
+     *
+     * @return \Majisti\ModelContainer
+     */
+    public function direct()
+    {
+        return Zend_Registry::get('Majisti_ModelContainer');
+    }
+}
