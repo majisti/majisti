8a59d78afb62aab18755da31ee55366d7e2ee19d
Steven Rosato <ratius@ratius-desktop.(none)>
2010-01-12 23:46:38 -0500

    Majisti\Util\Model\IViewAggregate will now return a view if the class' view is null and a view is registered under Zend_View in the Registry.

--- library/Majisti/Util/Model/ViewAggregate.php
+++ library/Majisti/Util/Model/ViewAggregate.php
@@ -5,19 +5,23 @@ namespace Majisti\Util\Model;
 /**
  * @desc Class that aggregates a Zend_View_Interface and provides
  * mutators and accessors.
- * 
+ *
  * @author Majisti
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  */
 class ViewAggregate implements IViewAggregate
 {
     protected $_view;
-    
+
     /**
      * @link IViewAggregate::getView()
      */
     public function getView()
     {
+        if( null === $this->_view && \Zend_Registry::isRegistered('Zend_View') ) {
+            return \Zend_Registry::get('Zend_View');
+        }
+
         return $this->_view;
     }
 
