c9f9dabbe4fff50911bf7d3a074934ab62759431
Steven Rosato <steven.rosato@majisti.com>
2010-01-15 16:20:02 -0500

    LocaleSession is not a Singleton on its own. Considering to remove the SingletonAbstract utility class which doesn't seem to be best practices.

--- library/Majisti/I18n/LocaleSession.php
+++ library/Majisti/I18n/LocaleSession.php
@@ -7,27 +7,27 @@ namespace Majisti\I18n;
  * the internationalisation of an application by populating the class with the
  * application's supported languages and default language which were all defined
  * in a configuration.
- * 
+ *
  * It is then possible to switch amongst the locales at any time.
- * 
+ *
  * TODO: review doc
- * 
+ *
  * @author Majisti
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  */
-class LocaleSession extends \Majisti\Util\Pattern\SingletonAbstract implements ILocale
+class LocaleSession implements ILocale
 {
-    /** 
-     * @var \Zend_Session_Namespace 
+    /**
+     * @var \Zend_Session_Namespace
      */
     protected $_session;
-    
+
     static protected $_instance;
-    
+
     /**
      * @desc Constructs a new Internationalisation object which will handle
      * the locale through \Zend_Locale automatically using the session.
-     * 
+     *
      * Based on the application's configuration, it will populate the supported locales.
      */
     protected function __construct()
@@ -35,41 +35,55 @@ class LocaleSession extends \Majisti\Util\Pattern\SingletonAbstract implements I
     	$this->_session = new \Zend_Session_Namespace('Majisti_I18n', true);
     	$this->reset();
     }
-    
+
+    /**
+     * @desc Returns the single instance of this class
+     *
+     * @return \Majisti\I18n\LocaleSession
+     */
+    static public function getInstance()
+    {
+        if( null === static::$_instance ) {
+            static::$_instance = new static();
+        }
+
+        return static::$_instance;
+    }
+
 	/**
      * @desc Flushes all I18n persistence and puts back defaults.
-     * @return I18n this
+     * @return \Majisti\I18n\LocaleSession this
      */
     public function reset()
     {
     	$session 	= $this->_session;
     	$config 	= \Zend_Registry::get('Majisti_Config');
-    	
+
     	if( !(isset($session->locales) && isset($session->defaultLocale)) ) {
     	    $selector = new \Majisti\Config\Selector($config);
-    	    
+
     	    $defaultLocale     = $selector->find('plugins.i18n.defaultLocale', 'en');
             $currentLocale     = $selector->find('plugins.i18n.currentLocale', $defaultLocale);
-            $supportedLocales  = $selector->find('plugins.i18n.supportedLocales', 
+            $supportedLocales  = $selector->find('plugins.i18n.supportedLocales',
                 new \Zend_Config(array()))->toArray();
-           
+
     	    $session->unlock();
-    	    
+
     	    $session->defaultLocale = $session->currentLocale = $defaultLocale;
             $session->locales       = $supportedLocales;
-            
+
             $this->_registerLocales($config);
             $this->_registerLocaleObject();
-            
+
             $session->lock();
     	}
-        
+
         return $this;
     }
-    
+
     /**
      * @desc Registers the current locale within a Zend_Locale object.
-     * 
+     *
      * In any case, a \Zend_Locale will be available is the registry
      * under the Zend_Locale key
      */
@@ -79,7 +93,7 @@ class LocaleSession extends \Majisti\Util\Pattern\SingletonAbstract implements I
         \Zend_Locale::setDefault($currentLocale);
         \Zend_Registry::set('Zend_Locale', new \Zend_Locale($currentLocale));
     }
-    
+
     /**
      * @desc Registers the default locale among with all the supported locales defined by
      * the application's configuration. The default locale will always be
@@ -88,31 +102,31 @@ class LocaleSession extends \Majisti\Util\Pattern\SingletonAbstract implements I
     protected function _registerLocales(\Zend_Config $config)
     {
         $selector = new \Majisti\Config\Selector($config);
-        
+
         $defaultLocale = $selector->find('defaultLocale', 'en');
-        
+
     	$this->_session->defaultLocale = $defaultLocale;
-    	
+
     	if( $supportedLocales = $selector->find('supportedLocales', false) ) {
     		$this->_session->locales = $supportedLocales->toArray();
     	}
-    	
+
     	array_unshift($this->_session->locales, $defaultLocale);
     }
-    
+
     /**
-     * @desc Returns the current locale. 
-     * The current locale persists through the session. 
-     * 
+     * @desc Returns the current locale.
+     * The current locale persists through the session.
+     *
      * @return String The current locale
-     * 
+     *
      * @see switchLocale() To switch the current locale
      */
     public function getCurrentLocale()
     {
         return $this->_session->currentLocale;
     }
-    
+
     /**
      * @desc Returns the default locale
      * @return string The default locale
@@ -121,19 +135,19 @@ class LocaleSession extends \Majisti\Util\Pattern\SingletonAbstract implements I
     {
     	return $this->_session->defaultLocale;
     }
-    
+
     /**
-     * @desc Returns all the supported locales including the default locale 
-     * defined by the application's configuration. The default locale 
+     * @desc Returns all the supported locales including the default locale
+     * defined by the application's configuration. The default locale
      * is always the first element of the array.
-     * 
+     *
      * @return Array All the supported locales, exclusing the default locale
      */
     public function getLocales()
     {
     	return $this->_session->locales;
     }
-    
+
     /**
      * @desc Returns only the supported locale as an array, omitting
      * the default locale
@@ -145,9 +159,9 @@ class LocaleSession extends \Majisti\Util\Pattern\SingletonAbstract implements I
     	array_shift($locales);
     	return $locales;
     }
-    
+
     /**
-     * @desc Returns whether the current application's locale is also the 
+     * @desc Returns whether the current application's locale is also the
      * default locale defined by the application's configuration.
      *
      * @return bool True is the current locale is also the default locale.
@@ -156,17 +170,17 @@ class LocaleSession extends \Majisti\Util\Pattern\SingletonAbstract implements I
     {
         return $this->getCurrentLocale() === $this->getDefaultLocale();
     }
-    
+
     /**
      * @desc Toggles between the registered locales. Switching is circular,
      * meaning that switching between the languages will never come to an end.
-     * 
-     * @param String $locale (optionnal def=null) Directly switch to that locale 
-     * and sets the pointer to that locale. An abbreviation must be passed 
+     *
+     * @param String $locale (optionnal def=null) Directly switch to that locale
+     * and sets the pointer to that locale. An abbreviation must be passed
      * (ex: fr, fr_CA) depending on what was setup in the configuration.
-     * 
+     *
      * @throws Exception If the locale given is not supported by this application.
-     * 
+     *
      * @return The next locale or the given locale if it was passed
      * as parameter.
      */
@@ -174,31 +188,31 @@ class LocaleSession extends \Majisti\Util\Pattern\SingletonAbstract implements I
     {
     	$session = $this->_session;
     	$locales = $this->getLocales();
-    	
+
     	$localeToSearch = null === $locale
     		? $this->getCurrentLocale()
     		: $locale;
-    		
+
     	$key = array_search($localeToSearch, $locales);
-    	
+
 		if( false === $key ) {
 			throw new Exception("Locale {$localeToSearch} is
 				not supported by this application");
 		}
-		
+
 		if( null === $locale ) {
 			$key = $key + 1 >= count($locales)
 				? 0
 				: $key + 1;
 		}
-		
+
 		$session->unlock();
 		$session->currentLocale = $locales[$key];
 		$session->lock();
-		
+
 		return $session->currentLocale;
     }
-    
+
     /**
      * @desc Returns whether the given local is supported by this application.
      *
