f1ae70e9652d67fb74efc1226f0d8064e3adc8a4
Steven Rosato <steven.rosato@majisti.com>
2010-01-24 16:38:33 -0500

    Added tests for Config\Handler\MarkupTest and found and fixed a bug.

--- tests/library/Majisti/Config/Handler/MarkupTest.php
+++ tests/library/Majisti/Config/Handler/MarkupTest.php
@@ -8,20 +8,53 @@ class MarkupTest extends \Majisti\Test\PHPUnit\TestCase
 {
     static protected $_class = __CLASS__;
 
+    /**
+     * @var Markup
+     */
     public $handler;
+
+    /**
+     * @desc Will become a Zend_Config after setup
+     *
+     * @var \Zend_Config
+     */
+    public $config = array(
+        'foo' => array(
+            'title' => 'Foo Title'
+        ),
+        'bar' => array(
+            'title' => 'Bar Title'
+        )
+    );
 
     public function setUp()
     {
+        $this->handler  = new Markup();
+        $this->config   = new \Zend_Config(
+            $this->config, array(
+                'allowModifications' => true
+            )
+        );
 
+        $this->handler->push(\Zend_Markup::factory('Bbcode'));
     }
 
     /**
      * @desc Asserts that every node that uses markups be replaced
      * with their proper text
      */
-    public function testHandle()
+    public function testHandleWithNoMarkupSyntaxReturnsSameConfig()
     {
-        $this->markTestIncomplete();
+        $config = $this->handler->handle($this->config);
+        $this->assertEquals($this->config->toArray(), $config->toArray());
+    }
+
+    public function testHandleWithMarkupSyntaxParsesCorrectly()
+    {
+        $this->config->foo->title = '[b]Foo Title[/b]';
+
+        $config = $this->handler->handle($this->config);
+        $this->assertEquals('<strong>Foo Title</strong>', $config->foo->title);
     }
 }
 
