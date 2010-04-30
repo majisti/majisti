43b8b6e7afeaf669b853cef0b3a42181e4e4f7e9
Steven Rosato <steven.rosato@majisti.com>
2010-04-30 11:07:50 -0400

    Test case for Application\Bootstrap completed.

--- library/Majisti/Application/Bootstrap.php
+++ library/Majisti/Application/Bootstrap.php
@@ -12,21 +12,16 @@ class Bootstrap extends \Zend_Application_Bootstrap_Bootstrap
 {
     /**
      * @desc Inits the application's library autoloader
-     * which is basically the same a an module autoloader.
+     * which is basically the same a module autoloader.
      *
      * @return \Zend_Application_Module_Autoloader
      */
     protected function _initLibraryAutoloader()
     {
-        $autoloader = \Zend_Loader_Autoloader::getInstance();
-        $libAutoloader = new \Zend_Application_Module_Autoloader(array(
+        return new \Zend_Application_Module_Autoloader(array(
             'basePath'  => APPLICATION_LIBRARY,
             'namespace' => APPLICATION_NAME
         ));
-
-        $autoloader->pushAutoloader($libAutoloader);
-
-        return $libAutoloader;
     }
 
     /**
