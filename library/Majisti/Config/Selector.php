f1a3f187b18496da1a94cddae6ebb7c974220dcf
Steven Rosato <steven.rosato@majisti.com>
2010-04-30 14:22:14 -0400

    Doc, tests and class comments for Config\Selector.

--- library/Majisti/Config/Selector.php
+++ library/Majisti/Config/Selector.php
@@ -2,38 +2,75 @@
 
 namespace Majisti\Config;
 
+/**
+ * @desc The config selector is a configuration finder that will
+ * find anything needed with a CSS like selection.
+ *
+ * E.g Finding foo.bar will return baz in array('foo' => array('bar' => 'baz'))
+ * @author Majisti
+ * @license
+ */
 class Selector
 {
+    /**
+     * @var \Zend_Config
+     */
     protected $_config;
-    
+
+    /**
+     * @desc Constructs a selector with the given config.
+     * @param \Zend_Config $config The config
+     */
     public function __construct(\Zend_Config $config = null)
     {
         $this->_config = $config;
     }
-    
+
+    /**
+     * @desc Finds a value within the config with the CSS like attribute
+     * selector. If a default return value is specified, that value will be
+     * returned when the value is not found with the specified selector.
+     * If not, an exception is thrown.
+     *
+     * @param string $selector The CSS like selector (e.g foo.bar.baz)
+     * @param string [optionnal] $returnDefault The default return value,
+     * keeping null will throw an exception when the value is not found
+     * with the specified selector.
+     *
+     * @throws \Majisti\Config\Exception If the value could not be found
+     * with the specified selector and that no default return value was given.
+     *
+     * @return string The found value
+     */
     public function find($selector, $returnDefault = null)
     {
-        $config = $this->getConfig();
-        $parts = explode('.' , (string)$selector);
-        
-        $currentSelection = '';
-        
+        $config             = $this->getConfig();
+        $parts              = explode('.' , (string)$selector);
+        $currentSelection   = '';
+
+        /* recursively search and return default value if not found and not null */
         foreach ($parts as $part) {
             $currentSelection .= $part . '.';
         	if( !isset($config->$part)) {
         	    if( null === $returnDefault ) {
-        	        rtrim($currentSelection, '.');
+        	        $formattedSelection = rtrim($currentSelection, '.');
+
             	    throw new Exception("Cannot find current selection
-                        [{$currentSelection}]");
+                        [{$formattedSelection}]");
         	    }
         	    return $returnDefault;
         	}
+
         	$config = $config->$part;
         }
         
         return $config;
     }    
-    
+
+    /**
+     * @desc Returns the config.
+     * @return \Zend_Config the config
+     */
     public function getConfig()
     {
         return $this->_config;
--- tests/library/Majisti/Config/SelectorTest.php
+++ tests/library/Majisti/Config/SelectorTest.php
@@ -27,13 +27,78 @@ class SelectorTest extends \Majisti\Test\PHPUnit\TestCase
      */
     public function setUp()
     {
-        $this->selector = new Selector();
-        $this->config   = new \Zend_Config(array());
+        $this->config   = new \Zend_Config(array(
+            'foo' => array(
+                'bar' => array(
+                    'baz' => 'value1'
+                ),
+            ),
+            'key1' => 'value3'
+        ));
+        $this->selector = new Selector($this->config);
     }
 
-    public function testFoo()
+    /**
+     * Asserts the constructor
+     */
+    public function test__construct()
     {
-        $this->markTestIncomplete();
+        $selector = new Selector();
+
+        $this->assertNull($selector->getConfig());
+
+        $selector = new Selector($this->config);
+        $this->assertEquals($this->config, $selector->getConfig());
+    }
+
+    /**
+     * @expectedException \Majisti\Config\Exception
+     */
+    public function test__constructWithNullConfigShouldThrowException()
+    {
+        $selector = new Selector();
+
+        $this->assertEquals('foo', $selector->find('foo.bar', 'foo'));
+        $selector->find('foo.bar');
+    }
+
+    /**
+     * @expectedException \Majisti\Config\Exception
+     */
+    public function testFind()
+    {
+        $selector = $this->selector;
+
+        $this->assertEquals('value1',   $selector->find('foo.bar.baz'));
+        $this->assertEquals('foo',      $selector->find('foo.bar.void', 'foo'));
+        $this->assertEquals('value3',   $selector->find('key1'));
+
+        /* throws exception */
+        $selector->find('foo.bar.void');
+    }
+
+    /**
+     * @expectedException \Majisti\Config\Exception
+     */
+    public function testSingleNonExistantKeyThrowsException()
+    {
+        $this->selector->find('void');
+    }
+
+    /**
+     * @expectedException \Majisti\Config\Exception
+     */
+    public function testNullReturnValueWillThrowException()
+    {
+        $this->selector->find('void', null);
+    }
+
+    /**
+     * @expectedException \Majisti\Config\Exception
+     */
+    public function testNULLReturnValue()
+    {
+        $this->selector->find('void', NULL);
     }
 }
 
