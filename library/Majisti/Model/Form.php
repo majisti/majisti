ea392dae363c6365f19192aa4aad958666072c2e
Steven Rosato <steven.rosato@majisti.com>
2010-06-09 17:33:32 -0400

    Added Label decorator for Zend_Forms. They now have a star before them when setRequired is used on its element. Also, the tooltip for the star is internationnalized within the Zend_Validate resource.

--- library/Majisti/Model/Form.php
+++ library/Majisti/Model/Form.php
@@ -12,6 +12,30 @@ namespace Majisti\Model;
 class Form extends \Zend_Form
 {
     /**
+	 * Constructs the form and load the translation according to the passed options if any or by
+	 * automatically applying the proper options if nothing was setup.
+	 *
+	 * @param Array $options
+	 */
+	public function __construct($options = null)
+	{
+		$this->addPrefixPath(
+            '\Majisti\Model\Form\Decorator\\',
+            'Majisti/Model/Form/Decorator', 'decorator'
+        );
+		$this->addElementPrefixPath(
+            '\Majisti\Model\Form\Decorator\\',
+            'Majisti/Model/Form/Decorator',
+            'decorator'
+        );
+		$this->addDisplayGroupPrefixPath(
+            '\Majisti\Model\Form\Decorator\\',
+            'Majisti/Model/Form/Decorator'
+        );
+
+		parent::__construct($options);
+	}
+    /**
      * @desc Overriding \Zend_Form setLayout function to allow the use
      * of Majisti ILayout objects in the form layout setting process.
      *
