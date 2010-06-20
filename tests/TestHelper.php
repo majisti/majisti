20d69970477213c73d2b5e5479bf9badf1dac5c3
Steven Rosato <steven.rosato@majisti.com>
2010-06-20 14:56:23 -0400

    Test helper is now wrapped in a minimal class instead of a plain PHP file. Of course, further extensibility should be added to make it easier for other applications to extend that TestHelper class.

--- tests/TestHelper.php
+++ tests/TestHelper.php
@@ -1,92 +1,181 @@
 <?php
 
+namespace Majisti;
+
+TestHelper::getInstance()->init();
+
 /**
- * @desc This file should be included once with every TestCase.
+ * @desc This file should be required once with every TestCase.
  *
  * @author Majisti
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  */
+class TestHelper
+{
+    static protected $_instance;
 
-/* set error reporting to the level to which the code must comply. */
-error_reporting( E_ALL | E_STRICT );
+    /**
+     * @var \Zend_Controller_Request_Http
+     */
+    protected $_request;
 
-/* set include paths */
-$majistiRoot = realpath(dirname(__FILE__) . '/../');
-$includePaths = array(
-    $majistiRoot,
-    "$majistiRoot/externals",
-    "$majistiRoot/library",
-    "$majistiRoot/tests",
-    "$majistiRoot/tests/externals",
-    "$majistiRoot/tests/library",
-    get_include_path()
-);
+    /**
+     * @return TestHelper
+     */
+    static public function getInstance()
+    {
+        if ( null === static::$_instance ) {
+            static::$_instance = new static();
+        }
 
-ini_set('memory_limit', '256M');
+        return static::$_instance;
+    }
 
-set_include_path(implode(PATH_SEPARATOR, $includePaths));
+    public function init()
+    {
+        $this->initErrorReporting();
+        $this->initIncludePaths();
+        $this->initPhpSettings();
+        $this->initDependencies();
+        $this->initAutoloaders();
+        $this->initXdebug();
+        $this->initMiscelleneous();
+    }
 
-/* include PHPUnit dependencies */
-$dependencies = array('Framework', 'Framework/IncompleteTestError',
-                      'Framework/TestCase', 'Framework/TestSuite',
-                      'Runner/Version', 'TextUI/TestRunner', 'Util/Filter');
+    /**
+     * @desc set error reporting to the level to which the code must comply.
+     */
+    protected function initErrorReporting()
+    {
+        error_reporting( E_ALL | E_STRICT );
+    }
 
-foreach ($dependencies as $dependency) {
-    require_once 'PHPUnit/' . $dependency . '.php';
-}
-
-/* autoloaders */
-require_once 'Zend/Loader/Autoloader.php';
-$loader = Zend_Loader_Autoloader::getInstance();
-
-require_once 'Majisti/Loader/Autoloader.php';
-$loader->pushAutoloader(new \Majisti\Loader\Autoloader());
-
-/* configure xdebug for performance, if the module is enabled */
-$request = new \Zend_Controller_Request_Http();
-if( extension_loaded('xdebug') ) {
-    if( !$request->has('d') ) {
-        xdebug_disable();
-    } else {
-        $params = array(
-            'xdebug.collect_params'             => 300,
-            'xdebug.var_display_max_data'       => 300,
-            'xdebug.var_display_max_children'   => 300,
-            'xdebug.var_display_max_depth'      => 300,
+    /**
+     * @desc Sets the required include paths
+     */
+    protected function initIncludePaths()
+    {
+        $majistiRoot = $this->getLibraryRoot();
+        $includePaths = array(
+            $majistiRoot,
+            "$majistiRoot/externals",
+            "$majistiRoot/library",
+            "$majistiRoot/tests",
+            "$majistiRoot/tests/externals",
+            "$majistiRoot/tests/library",
+            get_include_path()
         );
 
-        foreach ($params as $key => $value) {
-           ini_set($key, $value);
+        set_include_path(implode(PATH_SEPARATOR, $includePaths));
+    }
+
+    protected function initPhpSettings()
+    {
+        ini_set('memory_limit', '256M');
+    }
+
+    /**
+     * @desc Include all required dependencies
+     */
+    protected function initDependencies()
+    {
+        /* PHPUnit */
+        $phpunit = array('Framework', 'Framework/IncompleteTestError',
+                         'Framework/TestCase', 'Framework/TestSuite',
+                         'Runner/Version', 'TextUI/TestRunner', 'Util/Filter');
+
+        foreach ($phpunit as $file) {
+            require_once 'PHPUnit/' . $file . '.php';
         }
     }
+
+    /**
+     * @desc Init autoloaders
+     */
+    protected function initAutoloaders()
+    {
+        /* Zend */
+        require_once 'Zend/Loader/Autoloader.php';
+        $loader = $toUnset[] = \Zend_Loader_Autoloader::getInstance();
+
+        /* Majisti */
+        require_once 'Majisti/Loader/Autoloader.php';
+        $loader->pushAutoloader(new \Majisti\Loader\Autoloader());
+    }
+
+    protected function initXdebug()
+    {
+        /* configure xdebug for performance, if the module is enabled */
+        $request = $this->getRequest();
+        if( extension_loaded('xdebug') ) {
+            if( !$request->has('d') ) {
+                xdebug_disable();
+            } else {
+                $params = array(
+                    'xdebug.collect_params'             => 300,
+                    'xdebug.var_display_max_data'       => 300,
+                    'xdebug.var_display_max_children'   => 300,
+                    'xdebug.var_display_max_depth'      => 300,
+                );
+
+                foreach ($params as $key => $value) {
+                   ini_set($key, $value);
+                }
+
+                /* code coverage filtering */
+                PHPUnit_Util_Filter::addDirectoryToFilter($majistiRoot . '/externals');
+
+                foreach (array('php', 'phtml') as $suffix) {
+                    PHPUnit_Util_Filter::addDirectoryToFilter($majistiRoot . '/tests', ".$suffix");
+                }
+
+                PHPUnit_Util_Filter::addFileToFilter($majistiRoot .
+                    '/library/Majisti/Application/Constants.php');
+            }
+        }
+    }
+
+    protected function initMiscelleneous()
+    {
+        $majistiRoot = $this->getLibraryRoot();
+        $request     = $this->getRequest();
+
+        /* instanciate a mock application */
+        define('MAJISTI_FOLDER_NAME', dirname($majistiRoot));
+        define('APPLICATION_NAME', 'Majisti_Test');
+        define('APPLICATION_ENVIRONMENT', 'development');
+
+        \Majisti\Application::setApplicationPath(
+            $majistiRoot . '/tests/library/Majisti/Application/_project/application');
+        \Majisti\Application::getInstance()->bootstrap();
+
+        /* be a little bit more verbose according to request param */
+        if( $request->has('v') ) {
+            \Majisti\Test\Runner::setDefaultArguments(array(
+                'printer' => new \Majisti\Test\Listener\Simple\Html(null, true)
+            ));
+        }
+
+        \Zend_Session::$_unitTestEnabled = true;
+    }
+
+    /**
+     * @return \Zend_Controller_Request_Http
+     */
+    public function getRequest()
+    {
+        if( null == $this->_request ) {
+            $this->_request = new \Zend_Controller_Request_Http();
+        }
+
+        return $this->_request;
+    }
+
+    /**
+     * @return string The library's root
+     */
+    public function getLibraryRoot()
+    {
+        return realpath(dirname(__FILE__) . '/../');
+    }
 }
-
-/* instanciate a mock application */
-define('MAJISTI_FOLDER_NAME', dirname($majistiRoot));
-define('APPLICATION_NAME', 'Majisti_Test');
-define('APPLICATION_ENVIRONMENT', 'development');
-
-\Majisti\Application::setApplicationPath(
-    $majistiRoot . '/tests/library/Majisti/Application/_project/application');
-\Majisti\Application::getInstance()->bootstrap();
-
-/* code coverage filtering */
-PHPUnit_Util_Filter::addDirectoryToFilter($majistiRoot . '/externals');
-
-foreach (array('php', 'phtml') as $suffix) {
-    PHPUnit_Util_Filter::addDirectoryToFilter($majistiRoot . '/tests', ".$suffix");
-}
-
-PHPUnit_Util_Filter::addFileToFilter($majistiRoot .
-    '/library/Majisti/Application/Constants.php');
-
-/* be a little bit more verbose according to request param */
-if( $request->has('v') ) {
-    \Majisti\Test\Runner::setDefaultArguments(array(
-        'printer' => new \Majisti\Test\Listener\Simple\Html(null, true)
-    ));
-}
-
-\Zend_Session::$_unitTestEnabled = true;
-
-unset($dependencies, $majistiRoot, $loader, $includePaths, $request);
