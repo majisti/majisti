7fb6c47bd3f7defad60fb5f8ac40624a2183164b
Steven Rosato <steven.rosato@majisti.com>
2010-09-25 18:50:42 -0400

    Implemented Majisti Test's BaseUrl for helper.

--- tests/Majisti/View/Helper/Head/AbstractHeadOptimizerTest.php
+++ tests/Majisti/View/Helper/Head/AbstractHeadOptimizerTest.php
@@ -72,10 +72,8 @@ abstract class AbstractHeadOptimizerTest extends \Majisti\Test\TestCase
     public function setUp()
     {
         $this->filesPath = realpath(__DIR__ . '/../_files');
-
-        $options = $this->getHelper()->getOptions();
-        //FIXME: url should be retrieved via helper
-        $this->filesUrl  = '/majisti/tests/Majisti/View/Helper/_files';
+        $this->filesUrl  = $this->getHelper()->getMajistiBaseUrl() .
+            '/tests/Majisti/View/Helper/_files';
     }
 
     /**
