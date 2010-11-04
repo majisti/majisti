bba8a00c4155211298ab2dccba3a708eb53ccae3
Steven Rosato <steven.rosato@majisti.com>
2010-11-03 21:46:28 -0400

    Added headers view helper backported from a concrete project.

---
+++ libraries/Majisti/View/Helper/Headers.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Majisti\View\Helper;
+
+class Headers extends AbstractHelper
+{
+    /**
+     * @desc Returns the common headers for this application
+     *
+     * @return output
+     */
+    public function helper()
+    {
+        return $this;
+    }
+
+    public function prepare()
+    {
+        $view = $this->view;
+
+        $maj = $this->getConfig()->majisti;
+
+        $view->headLink()->appendStylesheet($maj->url . '/styles/common.css');
+        $view->headLink()->appendStylesheet($maj->app->url .
+            '/styles/default/default.css');
+    }
+
+    public function toString()
+    {
+        $view = $this->view;
+        $maj  = $this->getConfig()->majisti;
+
+        /* headers */
+        $headers = array();
+        $headers[] = $view->headMeta()->toString();
+        $headers[] = $view->headLink()->toString();
+        $headers[] = $view->headStyle()->toString();
+        $headers[] = trim($view->jQuery(), PHP_EOL);
+        $headers[] = $view->headScript()->toString();
+        $headers[] = $view->headTitle($maj->app->namespace);
+
+        /* append PHP_EOL on non empty strings */
+        $output = '';
+        foreach ($headers as $header) {
+            $output .= $header;
+
+        	if( !empty($header) ) {
+        	    $output .= str_repeat(PHP_EOL, 2);
+        	}
+        }
+
+        return $output;
+    }
+
+    public function __toString()
+    {
+        return $this->toString();
+    }
+}
