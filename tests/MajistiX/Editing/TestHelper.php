38ee918948b76f3b6ef26a3c2ebc24358d00666c
Steven Rosato <steven.rosato@majisti.com>
2010-12-24 21:25:21 -0500

    More tests for editing extension.

--- tests/MajistiX/Editing/TestHelper.php
+++ tests/MajistiX/Editing/TestHelper.php
@@ -5,4 +5,5 @@ namespace MajistiX\Editing;
 /* fallsback to parent test helper */
 require_once dirname(__DIR__) . '/TestHelper.php';
 
-\Majisti\Test\Helper::getInstance()->addExtension(__NAMESPACE__, __DIR__);
+$helper = \Majisti\Test\Helper::getInstance();
+$helper->addExtension(__NAMESPACE__, __DIR__);
