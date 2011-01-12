ef52d854009ae46837861b4cb6a4948de15f7e68
Steven Rosato <steven.rosato@majisti.com>
2011-01-12 00:57:44 -0500

    SCSS Refactor. SCSS can now be easily extended, along with images in concrete applications, leading to true css flexibility. Documentation will come.

--- libraries/MajistiX/Editing/scss/config.rb
+++
@@ -1,6 +0,0 @@
-sass_dir        = "."
-css_dir         = "../public/styles"
-images_dir      = "../public/images"
-line_comments   = false
-relative_assets = true
-
--- public/scss/config.rb
+++ public/scss/config.rb
@@ -1,6 +1,6 @@
 sass_dir        = "."
-css_dir         = "../../styles"
-images_dir      = "../../images"
+css_dir         = "../styles"
+images_dir      = "../images"
 line_comments   = false
 relative_assets = true
 
