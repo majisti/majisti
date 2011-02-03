6906b9649b46cb88184b4d32bba76c97cc491ebf
Steven Rosato <steven.rosato@majisti.com>
2011-02-03 17:32:52 -0500

    Removed Model Aggregators. Config might resurface as a Marker Interface such as Configurable, but that should be only the case of the big config refactor.

--- libraries/Majisti/Model/Model.php
+++ libraries/Majisti/Model/Model.php
@@ -2,32 +2,64 @@
 
 namespace Majisti\Model;
 
-class Model extends    \ArrayObject
-            implements \Majisti\Util\Model\Aggregator\IView
+/**
+ * @desc The majisti model.
+ *
+ * @author Majisti
+ */
+class Model extends \ArrayObject
 {
+    /**
+     * @var \Zend_View
+     */
     protected $_view;
 
+    /**
+     * @var mixed
+     */
     protected $_data;
 
+    /**
+     * @desc Constructs the model.
+     *
+     * @param mixed $data The data
+     * @param \Zend_View $view The view
+     */
     public function __construct($data = null, $view = null)
     {
         $this->_data = $data;
     }
 
+    /**
+     * @desc Returns the data.
+     *
+     * @return mixed The data
+     */
     public function getData()
     {
         return $this->_data;
     }
 
+    /**
+     * @desc Sets the view.
+     *
+     * @param \Zend_View_Interface $view The view
+     */
     public function setView(\Zend_View_Interface $view)
     {
         $this->_view = $view;
     }
 
+    /**
+     * @desc Returns the view. In case no view was provided, it attemps to retrieve
+     * a \Zend_View within the \Zend_Registry.
+     *
+     * @return \Zend_View The view
+     */
     public function getView()
     {
         if( null === $this->_view ) {
-            $this->_view = \Zend_Registry::get('Zend_View');
+            $this->setView(\Zend_Registry::get('Zend_View'));
         }
 
         return $this->_view;
