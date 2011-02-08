d9d9f9903d1a69673367c91135da26950b9901e4
Steven Rosato <steven.rosato@majisti.com>
2011-02-08 02:23:20 -0500

    Added fixes according to changes made in library.

--- libraries/MajistiX/Editing/views/helpers/PublicFiles.php
+++ libraries/MajistiX/Editing/views/helpers/PublicFiles.php
@@ -11,14 +11,22 @@ class PublicFiles extends \Majisti\View\Helper\AbstractHelper
         $view = $this->view;
 
         if( $publicFiles->has('scripts') ) {
-            foreach( $publicFiles->find('scripts') as $jsFile ) {
-                $view->headScript()->appendFile($jsFile);
+            foreach( $publicFiles->find('scripts') as $name => $jsFile ) {
+                if( is_int($name) ) {
+                    $view->headScript()->appendFile($jsFile);
+                } else {
+                    $view->headScript()->offsetSetFile($name, $jsFile);
+                }
             }
         }
 
         if( $publicFiles->has('styles') ) {
-            foreach( $publicFiles->find('styles') as $cssFile ) {
-                $view->headLink()->appendStylesheet($cssFile);
+            foreach( $publicFiles->find('styles') as $name => $cssFile ) {
+                if( is_int($name) ) {
+                    $view->headLink()->appendStylesheet($cssFile);
+                } else {
+                    $view->headLink()->offsetSetStylesheet($name, $cssFile);
+                }
             }
         }
     }
