ca36e4622f7df61328228c997d560651bd8798f8
Steven Rosato <steven.rosato@majisti.com>
2011-03-05 17:28:30 -0500

    Removed broken symbolic links. Switched version to 0.4.0beta2.

--- lib/Majisti/Version.php
+++ lib/Majisti/Version.php
@@ -7,7 +7,7 @@ class Version
     /**
      * Majisti Framework version identification - see compareVersion()
      */
-    const VERSION = '0.4.0beta';
+    const VERSION = '0.4.0beta2';
 
     /**
      * Compare the specified Majisti Framework version string $version
